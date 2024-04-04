41974dca54b1b4d7b496eccc8f611aed91a0550f
looiic <loic.jermann@gmail.com>
2024-04-04 17:12:07 +0200

    Fix navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,7 @@
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="/appartment">Appartment Regression</a>
+            <a class="nav-link" href="/house">House Regression</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/mnist">Number Classification</a>
